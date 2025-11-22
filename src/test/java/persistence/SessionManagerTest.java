040d397bdd76caecd87ce222dc167562536752db
Grace Hudgens <54286731+Grace0Hud@users.noreply.github.com>
2025-11-22 01:17:23 -0800

    additional session manager tests

--- src/test/java/persistence/SessionManagerTest.java
+++ src/test/java/persistence/SessionManagerTest.java
@@ -1,11 +1,16 @@
 package persistence;
 
+import model.Complexity;
+import model.Length;
+import model.StoryStrategy;
+import model.StrategyType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import persistence.Session;
 import persistence.SessionManager;
 
 import java.io.File;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,11 +38,21 @@ class SessionManagerTest
     @Test
     public void loadSessionTest()
     {
-        assertDoesNotThrow(()->{sm.loadSession("test");});
+        sm.saveSession(session, "test");
+        //making sure that the file exists.
+        assertDoesNotThrow(()->{
+            Session newSession = sm.loadSession("test");
+		assertEquals("Filler text.", newSession.getStory());
+            assertEquals(Complexity.CHILDFRIENDLY, newSession.getComplexity());
+            assertEquals(Length.SHORT, newSession.getLength());
+            assertEquals(StrategyType.FANTASY, newSession.getStoryStrategy());
+        });
     }
 
     @Test
     void getFilePath()
     {
+        sm.loadSession("test");
+        assertEquals(Path.of("save_files/test"), sm.getFilePath());
     }
 }
