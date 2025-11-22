d828af3a774ffe7383c46fea8d839377e19d0423
Grace Hudgens <54286731+Grace0Hud@users.noreply.github.com>
2025-11-22 01:10:27 -0800

    resolved test dependency issue and added session tests.

--- src/main/java/TerminalTest.java
+++ src/main/java/TerminalTest.java
@@ -1,5 +1,3 @@
-package main;
-
 import controller.MainController;
 import model.*;
 import persistence.Session;
--- src/test/java/persistence/SessionManagerTest.java
+++ src/test/java/persistence/SessionManagerTest.java
@@ -16,23 +16,28 @@ class SessionManagerTest
     @BeforeEach
     public void setup()
     {
-	  session = new Session();
-	  sm = new SessionManager();
+        session = new Session();
+        sm = new SessionManager();
     }
 
     @Test
     public void saveSessionTest()
     {
-	  //confirming that not exceptions are thrown.
-	  assertDoesNotThrow(()->{sm.saveSession(session, "test");});
-	  //asserting that the file exists.
-	  File file = new File("save_files/test");
-	  assertTrue(file.exists());
+        //confirming that not exceptions are thrown.
+        assertDoesNotThrow(()->{sm.saveSession(session, "test");});
+        //asserting that the file exists.
+        File file = new File("save_files/test");
+        assertTrue(file.exists());
     }
 
     @Test
     public void loadSessionTest()
     {
-	  assertDoesNotThrow(()->{sm.loadSession("test");});
+        assertDoesNotThrow(()->{sm.loadSession("test");});
+    }
+
+    @Test
+    void getFilePath()
+    {
     }
 }
